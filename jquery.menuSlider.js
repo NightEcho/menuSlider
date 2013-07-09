69d106592160e28a44318b29170a30351b8d32de
Justen Allan <justen.allan@gmail.com>
2013-07-08 18:51:54 -0700

    Update

--- jquery.menuSlider.js
+++ jquery.menuSlider.js
@@ -8,61 +8,69 @@
             borderColor  : "black"
         }, borderProps);
 
-        var bgProps = typeof bgProps !== 'undefined' ? bgProps : null;
+        var bgProps = $.extend({
+            bgColor      : null,
+            bgRadius     : null
+        }, bgProps);
 
         var trans = $.extend({
             speed        : ".4s",
             tStyle       : "ease"
         }, tProps);
 
-    	var menuItemPosition = $("#selected").position();
+        var list = $("ul", this);
+        var hoverItem;
 
-    	var list = $("ul", this);
-    	var hoverItem;
+        var menuItemPosition = $(".selectedMenuItem", list).position();
 
         this.append("<div class='menuSlider'></div>");
+        var slider = $(".menuSlider", this);
 
-        $(".menuSlider").css ({"width": $("#selected").width(),
-        		       "height": $("#selected").height(),
-        		       "position": "absolute",
-        		       "left": menuItemPosition.left,
-        		       "top": "0",
-        		       "pointer-events": "none",
-        		       "box-sizing": "border-box",
-        		       "cursor": "pointer",
-        		       "border-bottom": border.thickness + " " + border.borderStyle + " " + border.borderColor,
-			       "-webkit-transition": "left "+trans.speed+" "+trans.tStyle,
-			       "-moz-transition": "left "+trans.speed+" "+trans.tStyle,
-			       "-o-transition": "left "+trans.speed+" "+trans.tStyle,
-			       "transition": "left "+trans.speed+" "+trans.tStyle,
-			       "background-color": bgProps
-        		     });
+        $(slider).css ({"width": $(".selectedMenuItem", list).width(),
+                        "height": $(".selectedMenuItem", list).height(),
+                        "position": "absolute",
+                        "left": menuItemPosition.left,
+                        "top": "0",
+                        "pointer-events": "none",
+                        "box-sizing": "border-box",
+                        "cursor": "pointer",
+                        "border-bottom": border.thickness + " " + border.borderStyle + " " + border.borderColor,
+                        "-webkit-transition": "left "+trans.speed+" "+trans.tStyle,
+                        "-moz-transition": "left "+trans.speed+" "+trans.tStyle,
+                        "-o-transition": "left "+trans.speed+" "+trans.tStyle,
+                        "transition": "left "+trans.speed+" "+trans.tStyle,
+                        "background-color": bgProps.bgColor,
+                        "border-radius": bgProps.bgRadius
+        });
 
         //Move the Nav slider on hover
-	$("li", list).mouseenter(function() {
-		var pos = $(this).position();	
-		hoverItem = $(this);
-		$('.menuSlider').css ("left", pos.left);
-    	});
+        $("li", list).mouseenter(function() {
+            var pos = $(this).position();   
+            hoverItem = $(this);
+            $(slider).css ("left", pos.left);
+        });
 
-	//Return the Nav slider on exit
+        //Return the Nav slider on exit
         this.mouseleave(function() {
-		$('.menuSlider').css ("left", menuItemPosition.left);
-	});
+            $(slider).css ("left", menuItemPosition.left);
+        });
 
-	$(window).resize (function() {
-		menuItemPosition = $("#selected").position();	
-	        $(".menuSlider").css ({"width": $("#selected").width(),
-	                 	       "height": $("#selected").height(),
-	        		       "left": menuItemPosition.left,
-	        		     });
-	});
-	
-	//Allows click through to links on IE
-	this.click(function() {
-		var link = $("a", hoverItem).attr("href");
-		if (link !== undefined) document.location.href = link;
-	});
+        $(window).resize (function() {
+            menuItemPosition = $(".selectedMenuItem", list).position(); 
+            $(slider).css ({"width": $("#selected").width(),
+                            "height": $("#selected").height(),
+                            "left": menuItemPosition.left,
+            });
+        });
+
+        this.click(function() {
+
+            var link = $("a", hoverItem).attr("href");
+
+            if (link !== undefined) document.location.href = link;
+
+        });
+
     }
-    
+
 }(jQuery));
